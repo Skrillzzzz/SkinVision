ddf02c3d3e9fe9df0c79b3cf3c9ab7ecbe830211
Skrillzzzz <tunmise30@gmail.com>
2024-04-23 14:14:07 +0100

    Testing live server with Hello world

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from models import db
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/config.py
@@ -0,0 +1,4 @@
+class Config:   #database URI
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
